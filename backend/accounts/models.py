1bbb01cc6867b6db507768121889e1b56136f3e3
Pranav <pranav.mb.5221@gmail.com>
2026-08-10 15:55:27 +0530

    Add transaction model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,28 +2,40 @@ from django.conf import settings
 from django.db import models
 
 
-class AccountType(models.IntegerChoices):
+class TransactionType(models.IntegerChoices):
+    EXPENSE = 1, "Expense"
+    INCOME = 0, "Income"
+
+
+class Category(models.IntegerChoices):
     OTHER = 0, "Other"
-    BANK = 1, "Bank"
-    CASH = 2, "Cash"
-    CREDIT_CARD = 3, "Credit Card"
-
-
-class FinancialAccount(models.Model):
+    FOOD = 1, "Food"
+    SHOPPING = 2, "Shopping"
+    TRAVEL = 3, "Travel"
+class Transaction(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="accounts",
+        related_name="transactions",
     )
-
-    name = models.CharField(max_length=100)
-
-    account_type = models.IntegerField(
-        choices=AccountType.choices,
-        default=AccountType.OTHER
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
     )
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    transaction_type = models.IntegerField(
+        choices=TransactionType.choices
+    )
+    bank = models.CharField(max_length=100)
+    merchant = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+    category = models.IntegerField(
+        choices=Category.choices,
+        default=Category.OTHER
+    )
+    transaction_at = models.DateTimeField()
+    original_sms = models.TextField()
     def __str__(self):
-        return f"{self.name} ({self.user})"
+        return f"{self.transaction_type} - {self.amount} - {self.merchant}"
