2cba02d6f50e4ef414aa0c3fee593697922e1627
Pranav <pranav.mb.5221@gmail.com>
2026-09-20 10:13:15 +0530

    Add transaction API

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import health_check,register,user_login,me
+from .views import health_check,register,user_login,me,transactions
 
 urlpatterns = [
     path("health/", health_check),
     path("register/",register),
     path("login/",user_login),
     path("me/",me),
+    path("transactions/", transactions),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,TransactionSerializer
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
@@ -47,3 +47,25 @@ def me(request):
         "name": info.username,
         "email": info.email
     })
+
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def transactions(request):
+
+    if request.method == "POST":
+        serializer = TransactionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            transaction = serializer.save(user=request.user)
+
+            return Response(
+                TransactionSerializer(transaction).data,
+                status=201
+            )
+
+        return Response(serializer.errors)
+
+    transactions = request.user.transactions.all()
+    serializer = TransactionSerializer(transactions, many=True)
+
+    return Response(serializer.data)
